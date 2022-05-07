29d22d09721b55e74266373a499e8cdd03a55df7
NudurupatiSurya <nudurupatisurya@gmail.com>
2022-05-07 10:02:38 +0530

    7th May2022

---
+++ app/src/main/java/com/surya/indoolocationbasednavigation/Data.java
@@ -0,0 +1,14 @@
+package com.surya.indoolocationbasednavigation;
+
+public class Data {
+    String name,distance;
+
+  /*  public Data(String name, String distance) {
+        this.name = name;
+        this.distance = distance;
+    }*/
+    public Data(String name) {
+        this.name = name;
+        //this.distance = distance;
+    }
+}
---
+++ app/src/main/java/com/surya/indoolocationbasednavigation/List_Adapter.java
@@ -0,0 +1,39 @@
+package com.surya.indoolocationbasednavigation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class List_Adapter extends ArrayAdapter<Data> {
+    public List_Adapter(Context context, ArrayList<Data> datalist) {
+        super(context, R.layout.list_item, datalist);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Data data = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+        TextView name_tv = convertView.findViewById(R.id.list_item_tv_name);
+        TextView dist_tv = convertView.findViewById(R.id.list_item_tv_dist);
+        name_tv.setText(data.name);
+        dist_tv.setText(data.distance);
+
+        return super.getView(position, convertView, parent);
+    }
+}
